882d3582c638cf51238a074a3e8c7edc9f9f18b1
g4gamers1 <cappelle.kevin123@gmail.com>
2025-05-24 19:54:32 +0200

    bs containers

--- main.py
+++ main.py
@@ -5,20 +5,23 @@ from player import *
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    updatable_group = pygame.sprite.Group(player)
-    drawable_group = pygame.sprite.Group(player)
+
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+
     Player.containers = (updatable_group, drawable_group)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        for updatable in updatable_group:
-            updatable.update(dt)    
+        updatable_group.update(dt)    
         screen.fill("black")
         for drawable in drawable_group:
             drawable.draw(screen)
