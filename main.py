ec363569d5df35f7bbc134e49219c64077d5e6c3
g4gamers1 <cappelle.kevin123@gmail.com>
2025-05-24 18:59:45 +0200

    player movement

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-import pygame
+import pygame # type: ignore
 from constants import *
+from player import *
+
+
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable_group = pygame.sprite.Group(player)
+    drawable_group = pygame.sprite.Group(player)
+    Player.containers = (updatable_group, drawable_group)
     while True:
-        screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for updatable in updatable_group:
+            updatable.update(dt)    
+        screen.fill("black")
+        for drawable in drawable_group:
+            drawable.draw(screen)
         pygame.display.flip()
+
+
+        dt = clock.tick(240) / 1000.0
         
-            
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
